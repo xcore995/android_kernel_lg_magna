e05898df4f04d59445ea36c77150559ecac891b9
xcore995 <arman.p95@gmail.com>
2020-05-06 08:58:00 -0400

    Add mmap_rnd_bits and mmap_rnd_compat_bits
     * d07e22597d1d mm: mmap: add new /proc tunable for mmap_base ASLR
     * e0c25d958f78 arm: mm: support ARCH_MMAP_RND_BITS
     * 8f0d3aa9de57 arm64: mm: support ARCH_MMAP_RND_BITS
     * 9e08f57d684a x86: mm: support ARCH_MMAP_RND_BITS
     * ec9ee4acd97c drivers: char: random: add get_random_long()
     * 5ef11c35ce86 mm: ASLR: use get_random_long()

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1476,39 +1476,27 @@ static struct ctl_table vm_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_doulongvec_minmax,
 	},
-#ifdef CONFIG_MEMCG_ZNDSWAP
+#ifdef CONFIG_HAVE_ARCH_MMAP_RND_BITS
 	{
-		.procname	= "dt_swapcache",
-		.data		= &dt_swapcache,
-		.maxlen		= sizeof(dt_swapcache),
-		.mode		= 0644,
+		.procname	= "mmap_rnd_bits",
+		.data		= &mmap_rnd_bits,
+		.maxlen		= sizeof(mmap_rnd_bits),
+		.mode		= 0600,
 		.proc_handler	= proc_dointvec_minmax,
-		.extra1		= &zero,
+		.extra1		= (void *)&mmap_rnd_bits_min,
+		.extra2		= (void *)&mmap_rnd_bits_max,
 	},
+#endif
+#ifdef CONFIG_HAVE_ARCH_MMAP_RND_COMPAT_BITS
 	{
-		.procname	= "dt_writeback",
-		.data		= &dt_writeback,
-		.maxlen		= sizeof(dt_writeback),
-		.mode		= 0644,
+		.procname	= "mmap_rnd_compat_bits",
+		.data		= &mmap_rnd_compat_bits,
+		.maxlen		= sizeof(mmap_rnd_compat_bits),
+		.mode		= 0600,
 		.proc_handler	= proc_dointvec_minmax,
-		.extra1		= &zero,
+		.extra1		= (void *)&mmap_rnd_compat_bits_min,
+		.extra2		= (void *)&mmap_rnd_compat_bits_max,
 	},
-	{
-		.procname	= "dt_filecache",
-		.data		= &dt_filecache,
-		.maxlen		= sizeof(dt_filecache),
-		.mode		= 0644,
-		.proc_handler	= proc_dointvec_minmax,
-		.extra1		= &zero,
-	},
-	/*{
-		.procname	= "dt_free",
-		.data		= &dt_free,
-		.maxlen		= sizeof(dt_free),
-		.mode		= 0644,
-		.proc_handler	= proc_dointvec_minmax,
-		.extra1		= &zero,
-	},*/
 #endif
 	{ }
 };
